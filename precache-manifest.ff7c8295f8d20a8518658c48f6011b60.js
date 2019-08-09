f7d5c3ace63fd89ecffd346514de3dabfde32dd3
damzaky <dmzsylvester@gmail.com>
2019-08-09 09:46:14 +0700

    Updates

---
+++ precache-manifest.ff7c8295f8d20a8518658c48f6011b60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43ff5d35f81f095a9d0ed29a99becbf",
+    "url": "/dev-coba/index.html"
+  },
+  {
+    "revision": "052aa08c6e824b38a04b",
+    "url": "/dev-coba/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f66be8c63a8be590394c",
+    "url": "/dev-coba/static/js/2.fdc4b49b.chunk.js"
+  },
+  {
+    "revision": "052aa08c6e824b38a04b",
+    "url": "/dev-coba/static/js/main.26a76d76.chunk.js"
+  },
+  {
+    "revision": "b4c9c518ed97c8dca8b6",
+    "url": "/dev-coba/static/js/runtime~main.2da5a50f.js"
+  }
+]);
